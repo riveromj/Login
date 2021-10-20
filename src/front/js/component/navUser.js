34fa10701cddb23a9ee2939d97cb129dd0d92000
riveromj <marigen@gmail.com>
2021-10-20 14:03:02 +0000

    add user in navbar

--- src/front/js/component/navUser.js
+++ src/front/js/component/navUser.js
@@ -3,22 +3,31 @@ import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 import { useContext } from "react";
 import "../../styles/navUser.scss";
+import { useHistory } from "react-router-dom";
 
 export const NavUser = () => {
+	let history = useHistory();
 	const { store, actions } = useContext(Context);
-	console.log(store, "$$$$$");
+	console.log(store.user, "$$$$$");
+
+	const handleLogout = () => {
+		localStorage.removeItem("token");
+		actions.setPathName("/");
+		history.push("/");
+	};
 	return (
 		<nav className="navbar navbar-light bg-dark mb-3">
 			<Link to="/">
 				<span className="navbar-brand mb-0 h1">React Login/Signup</span>
 			</Link>
 			<div className="ml-auto m-2">
-				<span>{store.user}</span>
 				<Link to="/">
-					<button className="btn  btn-color">Loguot</button>
+					<button className="btn  btn-color" onClick={handleLogout}>
+						Log uot
+					</button>
 				</Link>
 			</div>
-			<div className="ml-auto">
+			<div className="text-user">
 				<span>{store.user}</span>
 			</div>
 		</nav>
