8bd3eed44da0550c7e9c5b98d19554298694473f
riveromj <marigen@gmail.com>
2021-10-20 14:16:49 +0000

    footer links

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -4,31 +4,34 @@ export const Footer = () => (
 	<footer className="bg-dark text-center text-white footer">
 		<div className="container p-4 pb-0">
 			<section className="mb-4">
-				<a className="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+				{/* <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button">
 					<i className="fab fa-facebook-f" />
-				</a>
+				</a> */}
 
-				<a className="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+				{/* <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button">
 					<i className="fab fa-twitter" />
-				</a>
+				</a> */}
 
-				<a className="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+				{/* <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button">
 					<i className="fab fa-google" />
-				</a>
-
+				</a> */}
+				{/* 
 				<a className="btn btn-outline-light btn-floating m-1" href="#!" role="button">
 					<i className="fab fa-instagram" />
-				</a>
+				</a> */}
 
-				<a className="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+				<a
+					className="btn btn-outline-light btn-floating m-1"
+					href="https://www.linkedin.com/in/riveromj"
+					role="button">
 					<i className="fab fa-linkedin-in" />
 				</a>
 
-				<a className="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+				<a className="btn btn-outline-light btn-floating m-1" href="https://github.com/riveromj" role="button">
 					<i className="fab fa-github" />
 				</a>
 			</section>
-			<div className="text-center p-3">© 2021 Copyright: @riverojm</div>
+			<div className="text-center p-3">© 2021 Copyright: @riveromj</div>
 		</div>
 	</footer>
 );
