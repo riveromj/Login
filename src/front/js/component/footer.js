1e6de62a22509f28286dacee2fac066483798e55
riveromj <marigen@gmail.com>
2021-10-20 12:08:05 +0000

    footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,3 +1,34 @@
 import React, { Component } from "react";
 
-export const Footer = () => <footer classNameName="footer mt-auto py-3 text-center" />;
+export const Footer = () => (
+	<footer className="bg-dark text-center text-white footer">
+		<div className="container p-4 pb-0">
+			<section className="mb-4">
+				<a className="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+					<i className="fab fa-facebook-f" />
+				</a>
+
+				<a className="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+					<i className="fab fa-twitter" />
+				</a>
+
+				<a className="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+					<i className="fab fa-google" />
+				</a>
+
+				<a className="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+					<i className="fab fa-instagram" />
+				</a>
+
+				<a className="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+					<i className="fab fa-linkedin-in" />
+				</a>
+
+				<a className="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+					<i className="fab fa-github" />
+				</a>
+			</section>
+			<div className="text-center p-3">© 2021 Copyright: @riverojm</div>
+		</div>
+	</footer>
+);
